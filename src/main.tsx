1a332019889e792dcdbd8e779e30dbeedc504dd2
shivanand <kshivananda534@gmail.com>
2025-11-19 22:26:20 +0530

    added page analytics packages

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,7 @@ import "./index.css";
 import Landing from "./pages/Landing.tsx";
 import "./types/global.d.ts";
 import { SpeedInsights } from "@vercel/speed-insights/react";
-import { Analytics } from "@vercel/analytics/next"
+import { Analytics } from "@vercel/analytics/react"
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
